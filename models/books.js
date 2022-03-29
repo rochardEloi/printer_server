4d3ff42658fc7777677782f7395e504e7a85c6f3
Rochard Eliamar Eloi <rochardeliamar.eloi@uniq.edu>
2022-03-29 12:54:45 -0400

    Add new Lines for books

--- models/books.js
+++ models/books.js
@@ -3,6 +3,11 @@ const bookSchema = mongoose.Schema({
     filename :{type : String, required : true},
     fs_id :{type : String, required : true},
 	user_id:{type : String, required : true},
+    buyer_details : {
+        lastname : {type:String},
+        firstname:{type:String},
+        email:{type:String}
+    },
 	status:{type : String, required : true},
     path_:{type : String, required : true},
 	page_number:{type : Number, required : true},
